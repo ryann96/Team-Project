6db24c00d2794f62dd14eedb7b3aa9e7710ff3c7
ZafeerA123 <142453937+ZafeerA123@users.noreply.github.com>
2023-12-07 07:44:56 -0800

    Update Thing1.js

--- assets/js/platformer/Thing1.js
+++ assets/js/platformer/Thing1.js
@@ -40,10 +40,10 @@ export class Thing1 extends GameObject {
     
         if (randomPosition) {
             thingX = (GameEnv.innerWidth - scaledWidth) / 2.5;
-            thingY = (GameEnv.innerHeight - scaledHeight) / 0.9;
+            thingY = (GameEnv.innerHeight - scaledHeight) / 1.0;
         } else {
             thingX = (GameEnv.innerWidth - scaledWidth) / 7.5;
-            thingY = (GameEnv.innerHeight - scaledHeight) / 2.3;
+            thingY = (GameEnv.innerHeight - scaledHeight) / 1.9;
         }
     
         // Set variables used in Display and Collision algorithms
